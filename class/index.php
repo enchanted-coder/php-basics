b8ee31e9a156fd453ab347904fcd757f97af540c
enchanted-coder <kosgeidaniel5216@gmail.com>
2023-02-19 02:30:18 +0300

    alright this will be hard but i removed the initial files added  a class directory which should contain new php files hopefully this merges well and i wont need to force on master lols lets see

---
+++ class/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>PHP docs</title>
+</head>
+<body style="background-color:antiquewhite;">
+<h1>hello</h1>
+<?php
+  $varName = "antiques";
+  echo $varName;
+?>
+</body>
+</html>
